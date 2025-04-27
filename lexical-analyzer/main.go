f8bb9a278decf2b6d29639db6aa2e387072b0526
naivash <n.ivaschenko@facct.ru>
2025-04-27 15:56:54 +0300

    add lexer based on finite state machine

--- lexical-analyzer/main.go
+++ lexical-analyzer/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 
+	"analyzer/fsmlex"
+	"analyzer/models"
 	"analyzer/rxlex"
 )
 
@@ -12,27 +13,29 @@ func main() {
 	var input []byte
 	var err error
 
-	if len(os.Args) > 1 {
+	if len(os.Args) > 2 {
 		input, err = os.ReadFile(os.Args[1])
 		if err != nil {
 			fmt.Println("Error reading file:", err)
 			return
 		}
-	} else {
-		input, err = io.ReadAll(os.Stdin)
-		if err != nil {
-			fmt.Println("Error reading stdin:", err)
-			return
+
+		var tokens []models.Token
+		if os.Args[2] == "fsm" {
+			tokens = fsmlex.Lex(string(input))
+		} else if os.Args[2] == "rx" {
+			tokens, err = rxlex.Lex(string(input))
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
 		}
+
+		for _, token := range tokens {
+			fmt.Printf("%s: %s\n", token.Type, token.Value)
+		}
+	} else {
+		fmt.Println("Not enough params. Example: lexer ./examples/example.go rx")
 	}
 
-	tokens, err := rxlex.Lex(string(input))
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	for _, token := range tokens {
-		fmt.Printf("%s: %s\n", token.Type, token.Value)
-	}
 }
